de58952df3abded5d01a10d465ebd3b86dc04999
iinow <iinow@naver.com>
2021-03-13 10:52:31 +0900

    핸들바 템플릿 엔진 추가

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('should return "Graphql Server"', () => {
+      expect(appController.root().message).toBe('Graphql Server')
     })
   })
 })
